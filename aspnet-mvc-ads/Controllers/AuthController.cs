831cfd4c85401457c6aabed610bc68f0e128a429
Sena <Sena@DESKTOP-4IMMO6J>
2023-02-16 21:06:17 +0300

    asdf

---
+++ aspnet-mvc-ads/Controllers/AuthController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_mvc_ads.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            var x= 12;
+            return View();
+        }
+    }
+}
