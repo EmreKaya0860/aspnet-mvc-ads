010f6d6af8060b1b4c1664a8e46a949e525a757c
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2023-02-16 21:00:34 +0300

    Kubra-1

---
+++ aspnet-mvc-ads/Controllers/PageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_mvc_ads.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
