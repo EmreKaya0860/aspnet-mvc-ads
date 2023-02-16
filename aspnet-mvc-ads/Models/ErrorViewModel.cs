e1c663f678438f4cd7eebe6991bc589959a9cbc4
EmreKaya0860 <h.emrekaya2207@gmail.com>
2023-02-16 20:55:07 +0300

    Add project files.

---
+++ aspnet-mvc-ads/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace aspnet_mvc_ads.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
