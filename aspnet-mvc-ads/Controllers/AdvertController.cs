1413a3508bef26b2cbc7004f090c91b94f5413e2
burcuustael <burcustael@gmail.com>
2023-02-16 21:15:19 +0300

    Advert Controller eklendi.

---
+++ aspnet-mvc-ads/Controllers/AdvertController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_mvc_ads.Controllers
+{
+    public class AdvertController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
