3c5653a5e28844f0cf29b386a9c35b53d33e6bae
Abdullah Başçi <abdullahbsc117@gmail.com>
2023-02-23 22:47:30 +0300

    User ve Page eklendi

--- App.Data/Entity/Page.cs
+++ App.Data/Entity/Page.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace App.Data.Entity
 {
-    internal class Page
+    public class Page
     {
+        public int Id { get; set; }
+
+
+        [Display(Name = "Başlık"), StringLength(200)]
+        public string Title { get; set; }
+
+
+
+        [Display(Name = "İçerik")]
+        public string Context { get; set; }
+
+
+
+        [Display(Name = "Durum")]
+        public bool IsActive { get; set; }
     }
 }
--- App.Data/Entity/User.cs
+++ App.Data/Entity/User.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace App.Data.Entity
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+
+
+        [Display(Name = "İsim"), StringLength(100)]
+        public string Name { get; set; }
+
+
+        [Display(Name = "Email"), StringLength(200)]
+        public string Email { get; set; }
+
+
+        [Display(Name = "Şifre"), StringLength(100)]
+        public string Password { get; set; }
+
+
+        [Display(Name = "Telefon"), StringLength(20)]
+        public int Phone { get; set; }
+
+
+        [Display(Name = "Adres"), StringLength(200)]
+        public string Address { get; set; }
+
+
+        public virtual List<Advert>? Adverts { get; set; }
+        public virtual List<Setting>? Settings { get; set; }
+
     }
 }
