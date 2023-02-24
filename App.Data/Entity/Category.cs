57f99f012f93267bb2aef8d434e678fe81120c3b
Sena <Sena@DESKTOP-4IMMO6J>
2023-02-24 14:13:57 +0300

    category&category advert eklendi

---
+++ App.Data/Entity/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace App.Data.Entity
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} alanı boş geçilemez!"), StringLength(100), Display(Name = "Kategori İsmi")]
+        public string Name { get; set; }
+        [StringLength(200), Display(Name = "Kategori Açıklaması")]
+        public string Description { get; set; }
+        public virtual List<CategoryAdvert>? CategoryAdverts { get; set; }
+    }
+}
---
+++ App.Data/Entity/CategoryAdvert.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Entity
+{
+    public class CategoryAdvert
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int AdvertId { get; set; }
+        public Advert Advert { get; set; }
+    }
+}
