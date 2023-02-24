b885878553ba83926cd4122aa9fb74381b697f64
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2023-02-24 09:19:32 +0300

    advert- setting entityler oluşturuldu

---
+++ App.Data/Entity/Advert.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Data.Entity
+{
+    public class Advert
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage = "{0} alanı boş geçilemez!"), StringLength(200), Display(Name = "İlan Başlığı")]
+        public string Title { get; set; }
+        [Display(Name = "İlan Açıklaması")]
+        public string Description { get; set; }
+
+        public int UserId { get; set; }
+       
+        public User? User { get; set; }
+
+
+
+        public virtual List<AdvertComment>? AdvertComments { get; set; }
+
+        public virtual List<AdvertImage>? AdvertImages { get; set; }
+
+        public virtual List<CategoryAdvert>? CategoryAdverts { get; set; }
+
+    }
+}
---
+++ App.Data/Entity/Setting.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Data.Entity
+{
+    public class Setting
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} alanı boş geçilemez!"), StringLength(200), Display(Name = "Ayar Adı ")]
+        public string Name { get; set; }
+
+        [Display(Name = "Ayar Değeri") , StringLength(200)]
+        public string Value { get; set; }
+
+        public int UserId { get; set; }
+
+        public User? User { get; set; }
+
+
+    }
+}
