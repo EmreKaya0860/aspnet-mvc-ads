b4810240f389ea4f78b0d3b0bdb4c0c9122016ab
burcuustael <burcustael@gmail.com>
2023-02-24 12:22:06 +0300

    AdvertImage ve AdvertComment class larının Data Annotations işlemleri yapılarak entity klasörüne eklendi.

---
+++ App.Data/Entity/AdvertComment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Entity
+{
+    public class AdvertComment 
+    {
+        public int Id { get; set; }
+
+        public int AdvertId { get; set; }
+
+        public Advert Advert { get; set; }
+
+        public int UserId { get; set; }
+
+        public User User { get; set; }  
+
+        [Display(Name="Yorum")]
+
+        public string? Comment { get; set; }
+
+        [Display(Name ="Durum")]
+        public bool IsActive { get; set; }
+    }
+}
