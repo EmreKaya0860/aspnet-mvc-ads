21d65cdd87e2a2779c7883d166c37ea16fef5668
KubraGavuzoglu <100765765+KubraGavuzoglu@users.noreply.github.com>
2023-03-06 09:56:10 +0300

    aa

---
+++ App.Data/Abstract/IAdvertListingRepository.cs
@@ -0,0 +1,14 @@
+﻿using App.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Data.Abstract
+{
+    public interface IAdvertListingRepository : IRepository<Advert>
+    {
+        int AddList(User user, Advert advert, List<AdvertImage> advertImage, List<Category> category);
+    }
+}
---
+++ App.Service/Abstract/IAdvertListingService.cs
@@ -0,0 +1,13 @@
+﻿using App.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Service.Abstract
+{
+    public interface IAdvertListingService : IAdvertListingRepository
+    {
+    }
+}
