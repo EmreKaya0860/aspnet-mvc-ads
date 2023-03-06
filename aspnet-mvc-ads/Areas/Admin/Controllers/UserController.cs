2132563f2d68c4263052d0e47f8ccbda3ed0d0af
burcuustael <burcustael@gmail.com>
2023-03-06 19:56:15 +0300

    Yönetim User Yönetimi sayfası CRUD işlemleri tamamlandı.

---
+++ aspnet-mvc-ads/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,111 @@
+﻿using App.Data.Entity;
+using App.Service.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_mvc_ads.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IService<User> _service;
+
+        public UserController(IService<User> service)
+        {
+            _service = service;
+        }
+
+        // GET: UserController
+        public ActionResult Index()
+        {
+            var model= _service.GetAll();
+            return View(model);
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.AddAsync(user);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+
+            }
+             return View(user);
+        }
+
+        // GET: UserController/Edit/5
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, User user)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _service.Update(user);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+             return View(user);
+        }
+
+        // GET: UserController/Delete/5
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model= await _service.FindAsync(id);
+            return View(model);
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, User user)
+        {
+            try
+            {
+                _service.Delete(user);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
