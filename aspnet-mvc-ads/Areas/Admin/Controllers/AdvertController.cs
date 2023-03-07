c7bd67ec6e8ad30f639336fc606f847df5fb2ee1
Abdullah Başçi <abdullahbsc117@gmail.com>
2023-03-08 00:31:38 +0300

    Admin için advert crud yapıları geliştirildi

---
+++ aspnet-mvc-ads/Areas/Admin/Controllers/AdvertController.cs
@@ -0,0 +1,103 @@
+﻿using App.Data.Entity;
+using App.Service.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_mvc_ads.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdvertController : Controller
+    {
+        private readonly IService<Advert> _service;
+
+        public AdvertController(IService<Advert> service)
+        {
+            _service = service;
+        }
+
+        public ActionResult Index()
+        {
+            var model = _service.GetAll();
+            return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateAsync(Advert advert)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _service.AddAsync(advert);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+
+            }
+            return View(advert);
+        }
+
+        public async Task<ActionResult> EditAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> EditAsync(int id, Advert advert)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _service.Update(advert);
+                    await _service.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "Hata Oluştu!");
+                }
+            }
+            return View(advert);
+        }
+
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            var model = await _service.FindAsync(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Advert advert)
+        {
+            try
+            {
+                _service.Delete(advert);
+                _service.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
