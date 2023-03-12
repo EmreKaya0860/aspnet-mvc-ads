6a3406967a7a566f89228a0a2905b952111d7283
burcuustael <burcustael@gmail.com>
2023-03-12 16:59:56 +0300

    ResetPassword denemeleri-1

---
+++ aspnet-mvc-ads/Models/ResetPasswordModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace aspnet_mvc_ads.Models
+{
+    public class ResetPasswordModel
+    {
+        [Required(ErrorMessage = "Bos birakalamaz")]
+        [EmailAddress(ErrorMessage = "Uygun fornatta degil")]
+
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Bos birakilanaz")]
+
+        [DataType(DataType.Password, ErrorMessage = "uygun değil")]
+
+        public string? NewPassword { get; set; }
+
+    }
+}
---
+++ aspnet-mvc-ads/Utils/ResetPassword.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Mail;
+
+namespace aspnet_mvc_ads.Utils
+{
+    public class ResetPassword
+    {
+        public static void PasswordSendMail(string link)
+
+        {
+            MailMessage mail = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            mail.From = new MailAddress("advertsite@gmail.com");
+            mail.To.Add("burcustael@gmail.com");
+            mail.Subject = "Sifre güncellene talebi";
+            mail.Body = "<h2>Şifrenizi yenileaek için Linke tiklayin </h2>";
+            mail.Body += $"<a href= '{link}' 1.> Şifre yenileme baglantısı";
+            mail.IsBodyHtml = true;
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.EnableSsl = true;
+            smtp.Credentials = new System.Net.NetworkCredential("burcustael@gmail.com", "123");
+            smtp.Send(mail);
+
+        }
+    }
+
+}
